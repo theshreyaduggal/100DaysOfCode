466c4c46faa842b8971897b18cb750cc9dbf1ab1
theshreyaduggal <duggalshreya2003@gmail.com>
2024-09-02 00:09:49 +0530

    LeetCode Questions

---
+++ Arrays/2022Convert1DArrayInto2DArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) 
+    {
+        if (m * n != original.length) 
+        {
+            return new int[0][0];
+        }
+        
+        int[][] array = new int[m][n];
+        int k = 0;
+
+        for (int i=0; i<m; i++) 
+        {
+            for (int j=0; j<n; j++) 
+            {
+                array[i][j] = original[k];
+                k++;
+            }
+        }
+
+        return array;    
+    }
+}
