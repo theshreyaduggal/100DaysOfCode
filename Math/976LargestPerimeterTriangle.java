0a5ede604ad04ce02d7f7d044cef306accb876a3
theshreyaduggal <duggalshreya2003@gmail.com>
2024-10-09 08:09:14 +0530

    LeetCode Questions

---
+++ Math/976LargestPerimeterTriangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestPerimeter(int[] nums) 
+    {
+        Arrays.sort(nums);
+    
+        for(int i=nums.length-1; i>1; i--)
+        {
+            int a = nums[i-2];
+            int b = nums[i-1];
+            int c = nums[i];
+			
+            int sum = a + b + c;
+        
+            if(a+b > c)
+		    {
+                return sum;
+            }
+        }
+    
+        return 0;    
+    }
+}
